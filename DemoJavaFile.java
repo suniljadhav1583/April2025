495e30d729b4cb298d6df37dae5b21b92f028917
suniljadhav1583 <suniljadhav1583@gmail.com>
2025-04-06 20:44:09 +0530

    Project Commit For First Time

---
+++ DemoJavaFile.java
@@ -0,0 +1,10 @@
+Java File
+
+
+echo "# April2025" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/suniljadhav1583/April2025.git
+git push -u origin main
